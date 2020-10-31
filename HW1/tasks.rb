05ef8b7c680134ca29a76b93fa0b02422133a473
Your Name <you@example.com>
2020-10-31 18:57:20 +0200

    refactor tasks.rb

--- HW1/tasks.rb
+++ HW1/tasks.rb
@@ -1,25 +1,35 @@
-p 'Task - 1'
-arr = (1..10).to_a
-p 'even index'
-arr.each_with_index do |val,index|
-  p val if (index % 2) == 0
-end
-p 'odd index'
-arr.each_with_index do |val,index|
-  p val if (index % 2) != 0
-end
-#---------------------------------
-p 'Task 2'
-arr = [1,2,3,4,5,6,7,8,9,10]
-even,odd = [],[]
-arr.each_with_index do |val,index|
-if (index % 2)!=0
-  odd << val
-  else even << val
+class My_Arr
+  def initialize
+    @arr = (1..10).to_a
+
+  end
+
+def task1
+  p 'even index'
+  @arr.each_with_index do |val,index|
+    p val if (index % 2) == 0
+  end
+  p 'odd index'
+  @arr.each_with_index do |val,index|
+    p val if (index % 2) != 0
   end
 end
-p 'Odd index'
-  odd.each {|odd| p odd}
-p 'Even index'
-  even.each {|even| p even}
 #---------------------------------
+def task2
+  even,odd = [],[]
+  @arr.each_with_index do |val,index|
+  if (index % 2)!=0
+    odd << val
+    else even << val
+    end
+  end
+  p 'Odd index'
+    odd.each {|odd| p odd}
+  p 'Even index'
+    even.each {|even| p even}
+end
+#---------------------------------
+
+
+less = My_Arr.new
+less.task2
