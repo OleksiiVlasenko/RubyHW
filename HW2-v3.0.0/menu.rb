294e6cb3e4dd1b431e0f2f3f96a82ff60cebf5f1
OleksiiVlasenko <a.a.a.vlasenko@gmail.com>
2020-11-26 11:50:05 +0200

    refact loggin

--- HW2-v3.0.0/menu.rb
+++ HW2-v3.0.0/menu.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
+
 require 'yaml'
 require_relative 'users/user'
 require_relative 'users/admin'
 require_relative 'users/superadmin'
 
 class Menu
-
   def start
     init_user
   end
@@ -18,11 +18,15 @@ class Menu
       p 'Введіть пароль'
       @pass = gets.chomp.downcase
       users.each do |user, pass|
-        User.new.start if @user == user.to_s && @pass == pass
-        SuperAdmin.new.start if @user == user.to_s && @pass == pass
-        Admin.new.start if @user == user.to_s && @pass == pass
+        login = @user.to_s if @user == user.to_s && @pass == pass
+        File.open("db/session.yml", "w") { |file| file.write(@user.to_yaml) }
+        Admin.new.start if login == 'admin'
+        User.new.start if login == 'guest'
+        SuperAdmin.new.start if login == 'super'
       end
+
     end
+
   end
 end
 
